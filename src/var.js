a044d150c242740348efb07e3835de1b7a701231
coderdivi <khosee6@gmail.com>
2023-03-13 23:52:11 +0100

    added dependencies

--- src/var.js
+++ src/var.js
@@ -1,3 +1,4 @@
+
 export const CLIENT_ID = "b3fa09d4e5ed4b3385c706e5b94dac18";
 export const REDIRECT_URI =  "http://localhost:3000/";
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
